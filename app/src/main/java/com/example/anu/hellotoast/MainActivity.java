2ee64b87cdf32eed2fd01ba54d423b03c04d8f52
Anusree <anusree@wizroots.com>
2017-11-20 15:47:37 +0530

    Show toast on clicking toast button, increment count and show on count text on clicking count button.

--- app/src/main/java/com/example/anu/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/anu/hellotoast/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.anu.hellotoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_count)
+    TextView tvCount;
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        tvCount.setText(String.valueOf(mCount));
     }
 }
